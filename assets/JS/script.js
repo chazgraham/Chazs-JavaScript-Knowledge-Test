fc8aeff85f0a93b40479538824b82ea557aaf7c1
chaz Graham <chazhg80.cg@gmail.com>
2022-08-07 15:56:39 -0500

    styled css

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,14 +1,29 @@
 var startQuizBtn = document.querySelector("#start-quiz");
 var test = document.querySelector("#test-content");
+var counter = 60
+
 
 var startQuiz = function() {
     startQuizBtn.remove();
+
     var testForm = document.createElement("div");
     testForm.textContent = ("test");
     testForm.className = "test-form";
     test.appendChild(testForm);
+    
+    var countdown = function() {
+    console.log(counter)
+    
+
+    counter--;
+    if(counter === 0 ) {
+        console.log("Times Up!")
+        clearInterval(countdown);
+    };
+    
+    
+}
 };
 
-
-
+var startCountdown = setInterval(countdown, 1000);
 startQuizBtn.addEventListener("click", startQuiz);
