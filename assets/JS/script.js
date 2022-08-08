f022ee94f1bc4683bbe5b750c559d5e8e076f4fe
chaz Graham <chazhg80.cg@gmail.com>
2022-08-08 15:54:41 -0500

    added correct data obj to array

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -91,9 +91,9 @@ var questionList = [
     {
         question: "placeholder10",
         answers: [
-            {text: "A1", correct: true},
+            {text: "A1", correct: false},
             {text: "A2", correct: false},
-            {text: "A3", correct: false},
+            {text: "A3", correct: true},
             {text: "A4", correct: false}
         ]
     }
@@ -119,12 +119,16 @@ function nextQuestion() {
 }
 
 function showQuestion(question) {
-    questionEl.innerText = question.question;
-    question.answers.forEach(answer => {
+    questionEl.innerText = question.question 
+    question.answers.forEach( function answer(answer) {
         var button = document.createElement("button");
         button.innerText = answer.text;
         button.classList.add("btn");
         
+        if(answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+
         button.addEventListener("click", selectAnswer);
         answerBtnEl.appendChild(button);
     });
@@ -136,10 +140,10 @@ function selectAnswer(event) {
     var selectedBtn = event.target;
     var correct = selectedBtn.dataset.correct;
     answerClass(document.body, correct)
-    Array.from(answerBtnEl.children).forEach(button => {
+    Array.from(answerBtnEl.children).forEach(function button(button) {
         answerClass(button, button.dataset.correct);
     })
-    
+
     console.log(selectedBtn)
 }
 
